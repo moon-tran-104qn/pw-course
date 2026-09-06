966ea1af2241cc931cf9cd2a72ee931e71b005f5
Moon Tran <moon-tran-104qn>
2026-09-06 10:22:39 +0700

    bai tap buoi 2

---
+++ tests/lesson-02/ex4.js
@@ -0,0 +1,9 @@
+const height = 165;
+
+if (height > 100){
+    const a = height - 100;
+    const ideal = a * 9 / 10;
+    const max = a;
+    const min = a * 8 / 10;
+    console.log("Cân nặng lý tưởng: ", ideal, "; Cân nặng tối đa: ", max, "; Cân nặng tối thiểu: ", min);
+}
